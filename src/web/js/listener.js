7a354e9cd266214760f880e600371129b1a3fabd
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-03-16 19:16:25 +0100

    carusell

--- src/web/js/listener.js
+++ src/web/js/listener.js
@@ -47,7 +47,7 @@ class listener {
 			document.querySelector('body>button[name="logoff"]').style.display = '';
 
 			var table = document.querySelector('event sortable-table');
-			table.list = [...events];
+			table.list = events;
 			table.style('tr.past{opacity:0.4;}tbody{max-height:18em;}');
 			if (!table.columns.length) {
 				var now = new Date();
@@ -149,7 +149,6 @@ class listener {
 					}
 				}
 				td.innerHTML = note || '&nbsp;';
-				list[ui.parents(td, 'tr').getAttribute('i')].note = note;
 			}
 			if (e.detail.type != 'read')
 				listener.updateCotacts();
