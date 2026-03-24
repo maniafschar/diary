762015583ed1119653aa56f182a611d11660bbb2
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-03-24 15:39:14 +0100

    rotate video image

--- src/web/js/element/ImageCarousel.js
+++ src/web/js/element/ImageCarousel.js
@@ -232,7 +232,7 @@ a {
 		var video = imageContainer.appendChild(document.createElement('video'));
 		video.controls = true;
 		video.autoplay = false;
-		video.playsinline = true;
+		video.setAttribute('playsinline', true);
 		video.appendChild(document.createElement('source')).type = 'video/mp4';
 		data.appendChild(document.createElement('description'));
 		var next = div.appendChild(document.createElement('button'));
