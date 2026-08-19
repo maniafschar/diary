3b9f7ace730625d07b2675ce5720a71cffdef2d2
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-19 13:41:01 +0200

    Fix rating selection logic and update star SVG fill

--- src/web/js/element/InputRating.js
+++ src/web/js/element/InputRating.js
@@ -100,7 +100,7 @@ ratingSelection span {
 		if (this.getAttribute('type') == 'edit') {
 			var e = this._root.querySelectorAll('ratingSelection > svg');
 			for (var i = 0; i < e.length; i++) {
-				if (i > x)
+				if (i < x)
 					e[i].classList.add('fill');
 				else
 					e[i].classList.remove('fill');
@@ -125,6 +125,6 @@ ratingSelection span {
 	}
 
 	star(no, full) {
-		return '<svg width="24" height="24" viewBox="0 0 16 16" stroke="currentColor"' + (no ? ' onclick="this.getRootNode().host.rate(' + no + ', true)"' : '') + '><style>.fill{stroke:transparent;fill:rgb(210, 225, 20)}</style><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path></svg>';
+		return '<svg width="24" height="24" viewBox="0 0 16 16" stroke="currentColor" fill="transparent"' + (no ? ' onclick="this.getRootNode().host.rate(' + no + ', true)"' : '') + '><style>.fill{stroke:rgb(200, 205, 0);fill:rgb(210, 225, 20)}</style><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path></svg>';
 	}
 }
