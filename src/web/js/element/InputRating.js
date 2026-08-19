e53452cdc2867bf49b0455f53981beb37a8cd03f
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-19 16:22:19 +0200

    Update InputRating.js

--- src/web/js/element/InputRating.js
+++ src/web/js/element/InputRating.js
@@ -25,6 +25,11 @@ class InputRating extends HTMLElement {
 	display: inline;
 }
 
+:host(.minimal) svg {
+	width: 1em;
+	height: 1em;
+}
+
 detailRating {
 	position: relative;
 	color: darkgoldenrod;
@@ -116,8 +121,8 @@ ratingSelection span {
 		var id = new Date().getTime() + Math.random();
 		return `<svg width="24" height="24" viewBox="0 0 16 16" stroke="currentColor"${no ? ' onclick="this.getRootNode().host.rate(' + no + ', true)"' : ''}>
 					<clipPath id="fillClip${id}"><rect x="0" y="0" width="${isNaN(x) ? 24 : x * 24 / 100}" height="24" /></clipPath>
-					<path clip-path="url(#fillClip${id})" d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
-					<path fill="none" stroke="#0000FF" stroke-width="3" stroke-linejoin="round" d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+					<path fill="rgb(210, 225, 20)" clip-path="url(#fillClip${id})" d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+					<path fill="none" stroke="#888" stroke-width="1" stroke-linejoin="round" d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
 				</svg>`;
 	}
 }
