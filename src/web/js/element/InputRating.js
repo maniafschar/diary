204366122e2f06646af9014c22b745584f2b4661
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-20 11:50:32 +0200

    refac

--- src/web/js/element/InputRating.js
+++ src/web/js/element/InputRating.js
@@ -93,7 +93,7 @@ ratingSelection svg {
 	star(no, x) {
 		var id = new Date().getTime() + Math.random();
 		return `<svg width="24" height="24" viewBox="0 0 16 16" stroke="currentColor"${no ? ' onclick="this.getRootNode().host.rate(' + no + ', true)"' : ''}>
-					<clipPath id="fillClip${id}"><rect x="0" y="0" width="${isNaN(x) ? 24 : x * 24 / 100}" height="24" /></clipPath>
+					<clipPath id="fillClip${id}"><rect x="0" y="0" width="${isNaN(x) ? 24 : x <= 0 ? 0 : x * 24 / 100}" height="24" /></clipPath>
 					<path fill="rgb(210, 225, 20)" clip-path="url(#fillClip${id})" d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
 					<path fill="none" stroke="#bbb" stroke-width="1" stroke-linejoin="round" d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
 				</svg>`;
