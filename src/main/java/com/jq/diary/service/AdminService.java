a982e755b03bf66548ba7b42730de3fd8541b77c
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-20 12:10:49 +0200

    refac

--- src/main/java/com/jq/diary/service/AdminService.java
+++ src/main/java/com/jq/diary/service/AdminService.java
@@ -1,5 +1,8 @@
 package com.jq.diary.service;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
@@ -7,7 +10,13 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 import org.apache.commons.io.IOUtils;
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -110,9 +119,33 @@ public class AdminService {
 							Utilities.scaleImage(Attachment.image(eventImage.getImage()), 150)));
 					this.repository.save(eventImage);
 					i++;
-				} catch (final Exception ex) {
-					result.append(Utilities.stackTraceToString(ex));
-					result.append("\n\n");
+				} catch (final Exception e) {
+					try {
+						final File videoFile = new File(Attachment.fullPath(eventImage.getImage()));
+						try {
+							// Holt das Frame an der gewünschten Sekunde
+							final Picture picture = FrameGrab.getFrameAtSec(videoFile, 0.0);
+
+							if (picture != null) {
+								// Konvertiert das JCodec-Picture in ein Java BufferedImage
+								final BufferedImage bufferedImage = AWTUtil.toBufferedImage(picture);
+
+								// Speichert das Bild als JPEG
+								final ByteArrayOutputStream out = new ByteArrayOutputStream();
+								ImageIO.write(bufferedImage, "jpg", out);
+								eventImage.setImageThumbnail(Attachment.createImage("jpg",
+										Utilities.scaleImage(out.toByteArray(), 150)));
+								this.repository.save(eventImage);
+								i++;
+							}
+						} catch (IOException | JCodecException e) {
+							System.err.println("Fehler beim Extrahieren des Thumbnails: " + e.getMessage());
+							e.printStackTrace();
+						}
+					} catch (final Exception ex) {
+						result.append(Utilities.stackTraceToString(ex));
+						result.append("\n\n");
+					}
 				}
 			}
 		}
