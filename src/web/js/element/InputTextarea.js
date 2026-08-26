8104b886b8139b99988fedc7ef8054a91641a12e
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-26 09:43:34 +0200

    Refactor speech button styling and functionality
    Removed background image from speech button and added SVG icon. Updated button color during speech recognition.

--- src/web/js/element/InputTextarea.js
+++ src/web/js/element/InputTextarea.js
@@ -35,8 +35,6 @@ textarea {
 }
 
 button.speech {
-	background-image: url(image/micro.svg);
-	background-size: 1.4em;
 	top: 0;
 	right: 0;
 	border-radius: 0 0.4em;
@@ -72,6 +70,9 @@ button.speech {
 			speechButton.classList.add('icon');
 			speechButton.classList.add('speech');
 			speechButton.onclick = this.captureAudio;
+			speechButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-20 0 122 122">
+  <path fill="currentColor" d="M59.89,20.83V52.3c0,27-37.73,27-37.73,0V20.83c0-27.77,37.73-27.77,37.73,0Zm-14.18,76V118.2a4.69,4.69,0,0,1-9.37,0V96.78a40.71,40.71,0,0,1-12.45-3.51A41.63,41.63,0,0,1,12.05,85L12,84.91A41.31,41.31,0,0,1,3.12,71.68,40.73,40.73,0,0,1,0,56a4.67,4.67,0,0,1,8-3.31l.1.1A4.68,4.68,0,0,1,9.37,56a31.27,31.27,0,0,0,2.4,12.06A32,32,0,0,0,29,85.28a31.41,31.41,0,0,0,24.13,0,31.89,31.89,0,0,0,10.29-6.9l.08-.07a32,32,0,0,0,6.82-10.22A31.27,31.27,0,0,0,72.68,56a4.69,4.69,0,0,1,9.37,0,40.65,40.65,0,0,1-3.12,15.65A41.45,41.45,0,0,1,70,85l-.09.08a41.34,41.34,0,0,1-11.75,8.18,40.86,40.86,0,0,1-12.46,3.51Z"/>
+</svg>`;
 			document.createElement('audio-player').controls = 'hidden';
 		}
 	}
@@ -112,6 +113,7 @@ button.speech {
 
 		this.speechRecognition.start();
 		this.isRecording = true;
+		this._root.querySelector('button').style.color = 'red';
 		console.log('Listening for speech...');
 	}
 
@@ -119,6 +121,7 @@ button.speech {
 		if (this.speechRecognition)
 			this.speechRecognition.stop();
 		this.isRecording = false;
+		this._root.querySelector('button').style.color = '';
 		console.log('Speech transcription stopped.');
 		this.speechRecognition = null;
 	}
