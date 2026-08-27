b3e48d9f0908daffd06500ac7e608d84f64102b8
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-27 18:08:46 +0200

    refac

--- src/web/js/element/ViewMap.js
+++ src/web/js/element/ViewMap.js
@@ -118,9 +118,9 @@ button {
 			var m = this._root.appendChild(document.createElement('div'));
 			m.style.flex = 1;
 			L.Marker.prototype.options.icon = L.icon({
-				iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
-				iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
-				shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png'
+				iconUrl: '/image/marker-icon.png',
+				iconRetinaUrl: '/image/marker-icon-2x.png',
+				shadowUrl: '/image/marker-shadow.png'
 			});
 			this.map = L.map(m, { zoomControl: true }).setView(
 				[this.locations[0].latitude, this.locations[0].longitude], 4);
