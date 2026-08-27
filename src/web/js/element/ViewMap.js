d57ce3f289fd55c2800a82a200c2bf157c41a327
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-27 18:36:14 +0200

    refac

--- src/web/js/element/ViewMap.js
+++ src/web/js/element/ViewMap.js
@@ -122,7 +122,7 @@ button {
 				iconRetinaUrl: '/image/marker-icon-2x.png',
 				iconSize: [34, 46],
 				iconAnchor: [20, 45],
-				popupAnchor: [-3, -40],
+				popupAnchor: [-3, -36],
 				shadowUrl: '/image/marker-shadow.png',
 				shadowSize: [60, 50],
 				shadowAnchor: [20, 45]
