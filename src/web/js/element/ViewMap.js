1c76f9b1dff4d334c065dd2a1ff5b5518eda4540
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-28 12:21:57 +0200

    Fix address formatting in marker popup
    Updated the address display in the popup to replace new lines with HTML line breaks.

--- src/web/js/element/ViewMap.js
+++ src/web/js/element/ViewMap.js
@@ -136,7 +136,7 @@ button {
 <div class="popup-box">
 	${loc.image ? `<img src="${loc.image}" alt="${this.escapeHtml(loc.name)}">` : ''}
 	<h3>${this.escapeHtml(loc.name)}</h3>
-	${`<div class="addr">${loc.date}${loc.address ? '<br/>' + this.escapeHtml(loc.address) : ''}</div>`}
+	${`<div class="addr">${loc.date}${loc.address ? '<br/>' + this.escapeHtml(loc.address).replace(/\n/g, '<br/>') : ''}</div>`}
 	${loc.note ? `<div class="note">${this.escapeHtml(loc.note)}</div>` : ''}
 </div>`);
 			marker.on('click', e => this.currentIndex = e.target.options.index);
