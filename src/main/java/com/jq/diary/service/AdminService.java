8968fa951a93b0272419597d513f4cc763cabe30
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-28 20:20:40 +0200

    Update AdminService.java

--- src/main/java/com/jq/diary/service/AdminService.java
+++ src/main/java/com/jq/diary/service/AdminService.java
@@ -118,7 +118,7 @@ public class AdminService {
 				return org.springframework.web.reactive.function.client.WebClient
 				.create("https://nominatim.openstreetmap.org/search?format=jsonv2&q=" + org.springframework.web.util.UriUtils.encode(location.getAddress(), StandardCharsets.UTF_8))
 				.get()
-				.accept(MediaType.APPLICATION_JSON)
+				.accept(org.springframework.http.MediaType.APPLICATION_JSON)
 				.header("user-agent",
 						"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36")
 				.retrieve().toEntity(String.class)
