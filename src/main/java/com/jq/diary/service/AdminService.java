f8c4b8b81cee0f8726814823e787b4994141626c
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-08-29 08:22:28 +0200

    Update AdminService.java

--- src/main/java/com/jq/diary/service/AdminService.java
+++ src/main/java/com/jq/diary/service/AdminService.java
@@ -113,9 +113,7 @@ public class AdminService {
 					createTicket(new Ticket(org.springframework.web.reactive.function.client.WebClient
 							.create("https://nominatim.openstreetmap.org/search?format=jsonv2&q=" + org.springframework.web.util.UriUtils.encode(location.getAddress().replace("\n", ", "), StandardCharsets.UTF_8))
 							.get()
-							.accept(org.springframework.http.MediaType.TEXT_PLAIN)
-							.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-							.header("Accept-Encoding", "gzip, deflate, br, zstd")
+							.header("Accept", "text/plain,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 							.header("Accept-Language", "en-US,en;q=0.9")
 							.header("user-agent",
 									"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36")
