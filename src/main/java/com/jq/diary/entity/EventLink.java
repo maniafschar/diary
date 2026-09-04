429b8e656e90da00f31ed411ebb5e7b010fe1ebf
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-04 10:10:39 +0200

    Update EventLink.java

--- src/main/java/com/jq/diary/entity/EventLink.java
+++ src/main/java/com/jq/diary/entity/EventLink.java
@@ -3,7 +3,7 @@ package com.jq.diary.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 import java.util.Date;
 import java.util.List;
