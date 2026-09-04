982d138439270bb6655d53625bbeaced6e182a92
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-04 09:54:21 +0200

    Create EventLink.java

---
+++ src/main/java/com/jq/diary/entity/EventLink.java
@@ -0,0 +1,59 @@
+package com.jq.diary.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class EventLink extends BaseEntity {
+	private List<Event> events;
+	@ManyToOne
+	private Contact contact;
+	private String email;
+	private String identifier;
+  private Date start;
+
+	public Contact getContact() {
+		return this.contact;
+	}
+
+	public void setContact(final Contact contact) {
+		this.contact = contact;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	public String getIdentifier() {
+		return this.identifier;
+	}
+
+	public void setIdentifier(final String identifier) {
+		this.identifier = identifier;
+	}
+
+	public Date getStart() {
+		return this.identifier;
+	}
+
+	public void setStart(final Date start) {
+		this.start = start;
+	}
+
+	public List<Event> getEvents() {
+		return this.events;
+	}
+
+	public void setEvents(final List<Event> events) {
+		this.events = events;
+	}
+}
