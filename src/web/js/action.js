29b939c64ebd324acddd1c631201655bc6538069
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-08 10:20:10 +0200

    Update action.js

--- src/web/js/action.js
+++ src/web/js/action.js
@@ -395,7 +395,7 @@ class action {
 	}
 
 	static resize() {
-		var mobile = parseFloat(getComputedStyle(document.body).fontSize) * 50 < window.innerWidth ? 0 : 5;
+		var mobile = parseFloat(getComputedStyle(document.body).fontSize) * 50 < window.innerWidth ? 0 : 3;
 		var diagonal = Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2));
 		var fontSize = (Math.min(7 + diagonal / 160, 26) + mobile);
 		if (mobile && fontSize > 18)
