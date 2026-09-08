8ad84aad60477ce3b9dd4dd0801e203778c2792d
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-08 13:25:15 +0200

    Update EventService.java

--- src/main/java/com/jq/diary/service/EventService.java
+++ src/main/java/com/jq/diary/service/EventService.java
@@ -47,8 +47,8 @@ public class EventService {
 	}
 
 	public List<String> listEmail(final BigInteger contactId) {
-		return this.repository.list("distinct email from EventLink where contact.id=?1 order by email asc", EventLink.class, contactId)
-				.stream().map(e -> e.getEmail()).collect(Collectors.toList());
+		return this.repository.list("from EventLink where contact.id=?1 order by email asc", EventLink.class, contactId)
+				.stream().map(e -> e.getEmail()).distinct().collect(Collectors.toList());
 	}
 
 	public void delete(final Event event) {
