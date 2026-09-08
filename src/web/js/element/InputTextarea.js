304adaec8be647384154515a0e71649dc22b3ab1
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-08 08:00:15 +0200

    speak

--- src/web/js/element/InputTextarea.js
+++ src/web/js/element/InputTextarea.js
@@ -71,7 +71,9 @@ button svg {
 	display: none;
 }`;
 		var textarea = this._root.appendChild(document.createElement('textarea'));
-		textarea.onkeyup = e => this.value = this._root.querySelector('textarea').value;
+		var f = e => this.value = this._root.querySelector('textarea').value;
+		textarea.addEventListener('keyup', f);
+		textarea.addEventListener('paste', f);
 		if (this.getAttribute('value')) {
 			textarea.value = this.getAttribute('value');
 			this.value = textarea.value;
