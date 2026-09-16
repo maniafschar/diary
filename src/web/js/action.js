9b97daffa6c57de986a6a543bcc2202c5d282503
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-16 17:41:58 +0200

    Fix formatting in action.js for button export display

--- src/web/js/action.js
+++ src/web/js/action.js
@@ -262,7 +262,7 @@ class action {
 		document.querySelector('element.calendar').style.display = '';
 		document.querySelector('element.user').style.display = '';
 		document.querySelector("element.event button.add").style.display = '';
-		document.querySelectorAll("element.event button.export").forEach(e => e.style.display = '' });
+		document.querySelectorAll("element.event button.export").forEach(e => e.style.display = '' );
 		document.querySelector('body>[name="logoff"]').style.display = 'none';
 		document.querySelector('body>[name="clientName"]').innerText = '';
 		ui.navigate(0);
