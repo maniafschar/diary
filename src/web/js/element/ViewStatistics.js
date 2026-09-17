0cbbb4905a858b7146a7a996edf65f1dcafe68df
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-17 08:57:41 +0200

    Add ViewStatistics custom element with shadow DOM

---
+++ src/web/js/element/ViewStatistics.js
@@ -0,0 +1,12 @@
+export { ViewStatistics };
+
+class ViewStatistics extends HTMLElement {
+	constructor() {
+		super();
+		this._root = this.attachShadow({ mode: 'open' });
+	}
+	connectedCallback() {
+		this._root.appendChild(document.createElement('style')).textContent = `
+`;
+	}
+}
