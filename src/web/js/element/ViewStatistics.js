be361e7bdfc9bdf3319efebce8dbf38816ccffe9
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-18 15:38:42 +0200

    Change event listener target to parentElement

--- src/web/js/element/ViewStatistics.js
+++ src/web/js/element/ViewStatistics.js
@@ -8,7 +8,7 @@ class ViewStatistics extends HTMLElement {
 		this._root = this.attachShadow({ mode: 'open' });
 	}
 	connectedCallback() {
-		this.addEventListener('visible', () => this.init(), { once: true });
+		this.parentElement.addEventListener('visible', () => this.init(), { once: true });
 		this._root.appendChild(document.createElement('style')).textContent = `
 `;
 	}
