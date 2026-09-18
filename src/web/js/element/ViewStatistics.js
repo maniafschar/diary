67504ee78c0586c7e617185d59fa51856aa4cd8d
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-18 15:18:24 +0200

    Simplify event listener options in ViewStatistics

--- src/web/js/element/ViewStatistics.js
+++ src/web/js/element/ViewStatistics.js
@@ -8,7 +8,7 @@ class ViewStatistics extends HTMLElement {
 		this._root = this.attachShadow({ mode: 'open' });
 	}
 	connectedCallback() {
-		this.addEventListener('visible', () => this.init(), { capture: false, passive: true, once: true });
+		this.addEventListener('visible', () => this.init(), { once: true });
 		this._root.appendChild(document.createElement('style')).textContent = `
 `;
 	}
