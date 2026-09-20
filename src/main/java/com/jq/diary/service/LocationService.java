1536af6d180803b809e80bfc50675d026c7875e9
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-20 09:24:44 +0200

    Update LocationService.java

--- src/main/java/com/jq/diary/service/LocationService.java
+++ src/main/java/com/jq/diary/service/LocationService.java
@@ -52,7 +52,7 @@ public class LocationService {
 	public void save(final Location location) {
 		if (location.getName() == null || location.getName().isBlank())
 			throw new IllegalArgumentException("Der Name der Location darf nicht leer sein.");
-		final Location locationStored = this.find(location);
+		final Location locationStored = this.find(location, location.getContact().getClient().getId());
 		if (locationStored == null) {
 			if (location.getLongitude() == null)
 				addGeoData(location);
