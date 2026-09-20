53bb692de0f3522c9bebe9946bcc67846a940506
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-20 20:11:44 +0200

    Fix import statement for FetchType in Contact.java

--- src/main/java/com/jq/diary/entity/Contact.java
+++ src/main/java/com/jq/diary/entity/Contact.java
@@ -7,7 +7,7 @@ import com.jq.diary.util.Utilities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
