aecd21999034cdbe2346f2fe7da381d1deca2ffa
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-24 09:56:03 +0200

    Update ViewStatistics.js

--- src/web/js/element/ViewStatistics.js
+++ src/web/js/element/ViewStatistics.js
@@ -130,7 +130,7 @@ chart bar {
 			.sort((entry, next) => entry.date - next.date);
 		if (!entries.length)
 			return;
-		var maxRating = 5;
+		var maxRating = 100;
 		var ratings = new Map(entries.map(entry => [this.dateKey(entry.date), entry]));
 		var firstDate = new Date(entries[0].date);
 		var lastDate = new Date();
