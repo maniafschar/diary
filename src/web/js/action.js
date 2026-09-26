8203f8ae0d0cac91b025ddceaa096c338727c01c
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-26 21:45:20 +0200

    summary

--- src/web/js/action.js
+++ src/web/js/action.js
@@ -486,7 +486,7 @@ class action {
 		document.dispatchEvent(new CustomEvent('popup', {
 			detail: {
 				body:
-					(summary.image ? '<img src="' + (summary.image.indexOf('.') > 0 ? '/med/' + summary.image : 'data:image/jpg;base64,' + summary.image) + '" style="max-width: 100%;"/>' : '') +
+					(summary.image ? '<img src="' + (summary.image.indexOf('.') > 0 ? '/med/' + summary.image : 'data:image/jpg;base64,' + summary.image) + '" style="max-width: 100%; border-radius: 0.5em;"/>' : '') +
 					'<div style="text-align: center; margin-bottom: 1em;"><div style="font-size: 2em;">' + summary.emojis.join('&nbsp; &nbsp;') + '</div>' + summary.adjectives.join(' · ') + '</div>' +
 					summary.note.replace(/\n/g, '<br/>')
 			}
