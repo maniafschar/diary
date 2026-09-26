b9d2a4ffe11048ea86704b4459d2a3183ed566f5
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2026-09-26 16:07:44 +0200

    color

--- src/web/js/element/ViewStatistics.js
+++ src/web/js/element/ViewStatistics.js
@@ -409,7 +409,7 @@ button:hover {
 	}
 
 	summaryDialog() {
-		document.dispatchEvent(new CustomEvent('popup', { detail: { body: '<input-selection></input-selection><div style="text-align: center; padding-top: 1.5em;"><button onclick="document.querySelector(&quot;view-statistics&quot;).summary()">KI fragen</button></div>' } }));
+		document.dispatchEvent(new CustomEvent('popup', { detail: { body: '<input-selection value="Summary"></input-selection><div style="text-align: center; padding-top: 1.5em;"><button onclick="document.querySelector(&quot;view-statistics&quot;).summary()">KI fragen</button></div>' } }));
 		var selection = document.querySelector('dialog-popup').content().querySelector('input-selection');
 		selection.add('Summary', 'Zusammenfassung');
 		selection.add('AdvicePsychology', 'Psychologische Empfehlung');
